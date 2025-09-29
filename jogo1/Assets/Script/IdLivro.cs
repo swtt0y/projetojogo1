ec55b5a2e6c2f4d041aa68389281b6fe40f0d15d
Ane <arianesotelo10@gmail.com>
2025-09-29 10:56:53 -0400

    arrumado

--- jogo1/Assets/Script/IdLivro.cs
+++ jogo1/Assets/Script/IdLivro.cs
@@ -11,39 +11,4 @@ public class IdLivro : MonoBehaviour, IPointerClickHandler
     {
         PuzzleLivros.Instance.PegarClick(idLivro);
     }
-    /*
-    private List<int> ordem = new List<int> {1, 2, 3, 4, 5, 6, 7, 8}; 
-    private List<int> tentativa = new List<int>();
-    private int limite = 8;
-
-    public void LerTentativa()
-    {
-        if (tentativa.Count < limite)
-        {
-            tentativa.Add(idLivro);
-            Debug.Log("adicionado");
-        }
-        if (tentativa.Count >= limite)
-        {
-            Comparar();
-        }
-    }
-    public void Comparar()
-    {
-        bool resultado = tentativa.SequenceEqual(ordem);
-        if (resultado)
-        {
-            LiberarChave();
-        }
-        else 
-        {
-            tentativa.Clear();
-        }
-    }
-    public void LiberarChave()
-    {
-        CameraColeta cameraColeta = Camera.main.GetComponent<CameraColeta>();
-        cameraColeta.ColetarObjeto(7);
-    }
-    */
 }
